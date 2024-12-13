29979c3a06b26fc397b3905f8cc1db7ea79267b3
phoenix <phoenixzero4@gmail.com>
2024-12-13 07:55:43 -0500

    first commit

---
+++ certification/basic/FizzBuzz.java
@@ -0,0 +1,24 @@
+package certification.basic;
+
+public class FizzBuzz {
+
+	public static void fizzbuzz(int n) {
+		
+		for(int i = 1; i <= n; i++) {
+		if(i % 3 == 0 && i % 5 == 0) {
+			System.out.println("FizzBuzz");
+		}else if(i % 5 == 0) {
+			System.out.println("Buzz");
+		}else if(i % 3 == 0) {
+			System.out.println("Fizz");
+		}else {
+			System.out.println(i);
+		}
+	}
+	}
+	public static void main(String[] args) {
+
+		fizzbuzz(15);
+	}
+
+}
---
+++ certification/basic/Main.java
@@ -0,0 +1,9 @@
+package certification.basic;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
